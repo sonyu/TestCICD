ca25e2b534b27d63ff19d41324fd47d4dc441966
khanh.vu@eastgate-software.com <khanh.vu@eastgate-software.com>
2023-04-10 13:27:53 +0700

    first commit

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- Hiển thị nội dung chào mừng từ Vue.js -->
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
